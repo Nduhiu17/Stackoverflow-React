85d32d0817f12caff5ae2a3d33e904e38e5425a0
nduhiu17 <nduhiu254@gmail.com>
2019-04-08 08:27:29 +0300

    Updates

---
+++ precache-manifest.8b1aca9db151eac595bf0acdfb48b861.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c044af137cd30721f5",
+    "url": "/Stackoverflow-React/static/js/runtime~main.a0c044af.js"
+  },
+  {
+    "revision": "b31918312b93c09e515f",
+    "url": "/Stackoverflow-React/static/js/main.b3191831.chunk.js"
+  },
+  {
+    "revision": "4b8eb41460b46940ed0b",
+    "url": "/Stackoverflow-React/static/js/1.4b8eb414.chunk.js"
+  },
+  {
+    "revision": "b31918312b93c09e515f",
+    "url": "/Stackoverflow-React/static/css/main.d03e3cb9.chunk.css"
+  },
+  {
+    "revision": "532b7ff60142ca70256c460e9e041086",
+    "url": "/Stackoverflow-React/index.html"
+  }
+];
--- src/dateFormat.js
+++
@@ -1,8 +0,0 @@
-import * as moment from "moment";
-
-
-const dateFormat = (dateString, format='MMMM Do YYYY, h:mm:ss a') => {
-    return moment(dateString).format(format);
-};
-
-export default dateFormat;
